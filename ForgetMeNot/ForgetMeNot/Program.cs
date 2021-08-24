8098f7524668219bb6a9d6ae817cfb116bd05a1e
ajameslarner <ajameslarner@gmail.com>
2021-08-24 22:27:04 +0100

    init

---
+++ ForgetMeNot/ForgetMeNot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForgetMeNot
+{
+    class Program
+    {
+
+        static string m_Hello = "Hello Jess"; 
+
+        static void Main(string[] args)
+        {
+            WriteConsole(m_Hello);
+        }
+
+        static void WriteConsole(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+    }
+}
+ 
